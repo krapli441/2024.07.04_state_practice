7db686c68722f8ee5901d49648ba4c9f37821c75
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-04 19:04:51 +0900

    todoList.tsx에 생성한 컴포넌트 추가

--- src/todoList.tsx
+++ src/todoList.tsx
@@ -2,11 +2,20 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
 import { todoListState } from "./state";
+import TodoItem from "./todoItem";
+import TodoItemCreator from "./todoItemCreator";
 
 const TodoList: React.FC = () => {
   const todoList = useRecoilValue(todoListState);
 
-  return <div></div>;
+  return (
+    <div>
+      <TodoItemCreator />
+      {todoList.map((todo, index) => (
+        <TodoItem key={index} item={todo} />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
