235dcf09c0459a02a32a8704ec9495929209460a
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-04 19:04:11 +0900

    todoItem 생성

---
+++ src/todoItem.tsx
@@ -0,0 +1,56 @@
+// src/TodoItem.tsx
+import React from "react";
+import { useRecoilState } from "recoil";
+import { todoListState, Todo } from "./state";
+
+interface TodoItemProps {
+  item: Todo;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ item }) => {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const index = todoList.findIndex((listItem) => listItem === item);
+
+  const editItemText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      text: e.target.value,
+    });
+    setTodoList(newList);
+  };
+
+  const toggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isComplete: !item.isComplete,
+    });
+    setTodoList(newList);
+  };
+
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(todoList, index);
+    setTodoList(newList);
+  };
+
+  return (
+    <div>
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isComplete}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>X</button>
+    </div>
+  );
+};
+
+function replaceItemAtIndex<T>(arr: T[], index: number, newValue: T): T[] {
+  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
+
+function removeItemAtIndex<T>(arr: T[], index: number): T[] {
+  return [...arr.slice(0, index), ...arr.slice(index + 1)];
+}
+
+export default TodoItem;
